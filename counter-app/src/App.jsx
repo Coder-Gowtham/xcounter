f320e05eb3c6a5c23336d8725d12aa876cb235c4
Gowtham <codewithgowtham.kv@gmail.com>
2026-07-03 15:14:46 +0530

    complete counter

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+
+      <p>Count: {count}</p>
+
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+
+      <button type="button" onClick={decrement}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
